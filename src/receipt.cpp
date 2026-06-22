823ac192b4c70acbe87dc1d4c6ad0addd972170d
yshybasdccd <yshyba@student.sdccd.edu>
2026-06-22 02:29:31 +0000

    My Lab1 receipt calculator

--- src/receipt.cpp
+++ src/receipt.cpp
@@ -2,22 +2,22 @@
 
 // TODO: Return the sum of all three item prices.
 double calculateSubtotal(double item1, double item2, double item3) {
-    return 0.0;
+    return item1 + item2+ item3;
 }
 
 // TODO: Return the sales tax for the subtotal using TAX_RATE.
 double calculateTax(double subtotal) {
-    return 0.0;
+    return subtotal*TAX_RATE;
 }
 
 // TODO: Return the tip amount.
 // Example: if subtotal is 20.00 and tipPercent is 15,
 // the tip should be 3.00.
 double calculateTip(double subtotal, double tipPercent) {
-    return 0.0;
+    return subtotal* (tipPercent/100.0);
 }
 
 // TODO: Return the final total.
 double calculateTotal(double subtotal, double tax, double tip) {
-    return 0.0;
+    return subtotal + tax + tip;
 }
